859eeadc2d8d6547bf018f32eb020e6027ec5d8c
Soraia Novaes <sonovaes511@gmail.com>
2024-06-04 00:00:47 -0300

    Ajustado: texto Services

--- components/Services.tsx
+++ components/Services.tsx
@@ -11,9 +11,9 @@ const Services = () => {
 					<div data-aos="fade-right">
 						<div className="bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
 							<CodeBracketSquareIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]"/>
-							<h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Web Developer
+							<h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Frontend Web Developer
 							</h1>
-							<p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nostrum officiis, illo quisquam ut, dolore minima atque sed, dolor tempora aliquid totam alias molestias provident nemo at eligendi nulla hic?</p>
+							<p className="text-[15px] text-[#d3d2d2] font-normal">Tenho mais de cinco anos de experiência como desenvolvedora web front-end, criando websites dinâmicos e responsivos com frameworks como Svelte, Next.js, ReactJS e Vue.js. Especializo-me em desenvolvimento e manutenção de websites em WordPress, SEO, e-commerce e ERP. Minha experiência inclui gestão de hospedagem e manutenção de websites, garantindo entregas de alta qualidade para diversos clientes.</p>
 						</div>
 					</div>
 					<div data-aos="zoom-in" data-aos-delay="300">
@@ -21,15 +21,15 @@ const Services = () => {
 							<RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]"/>
 							<h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Product Owner
 							</h1>
-							<p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nostrum officiis, illo quisquam ut, dolore minima atque sed, dolor tempora aliquid totam alias molestias provident nemo at eligendi nulla hic?</p>
+							<p className="text-[15px] text-[#d3d2d2] font-normal">Como Product Owner, liderei o desenvolvimento do Criador de Aulas, uma plataforma de criação de aulas interativas. Colaborei com equipes multifuncionais para definir a visão estratégica, gerenciar o backlog e facilitar a comunicação entre stakeholders, resultando em mais de 6600 usuários e 500 aulas publicadas. Minha abordagem ágil e colaborativa assegurou entregas de alta qualidade e impactou positivamente a educação.</p>
 						</div>
 					</div>
 					<div data-aos="fade-left" data-aos-delay="500">
 						<div className="bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
 							<CommandLineIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae8]"/>
-							<h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Edu Technologies
+							<h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]"> Project Manager
 							</h1>
-							<p className="text-[15px] text-[#d3d2d2] font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet nostrum officiis, illo quisquam ut, dolore minima atque sed, dolor tempora aliquid totam alias molestias provident nemo at eligendi nulla hic?</p>
+							<p className="text-[15px] text-[#d3d2d2] font-normal">Tenho vasta experiência em gestão de projetos, coordenando a transição do Currículo Nacional para plataformas online e treinando milhares de professores em tecnologias digitais. Minha liderança e habilidades em design instrucional permitiram implementar programas de treinamento que melhoraram o engajamento das equipes. Também gerenciei projetos estratégicos com orçamentos multimilionários, colaborando com líderes executivos e equipes multifuncionais.</p>
 						</div>
 					</div>
 				</div>
